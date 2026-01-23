7a260d2b732a7103b524d2c7b913e38c68414f0c
yunusemrekmc <yek704@aof.anadolu.edu.tr>
2026-01-23 17:21:21 +0300

    better use uintmax_t then assume uint64_t is available

--- include/fs.h
+++ include/fs.h
@@ -1,6 +1,7 @@
 #ifndef FS_H
 #define FS_H
 
+#include <sys/stat.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <dirent.h>
@@ -14,13 +15,14 @@
 
 /* key for our hash table that stores each file */
 struct kfile {
-	uint64_t st_dev;	/* file device number */
-	uint64_t st_ino;	/* file inode number */
+	uintmax_t st_dev;	/* file device number */
+	uintmax_t st_ino;	/* file inode number */
 };
 
 struct file {
-	int pfd; 		/* parent fd */
-	char* path;		/* relative to pfd */
+	char* path;		/* relative to source */
+	mode_t mode; 		/* file mode */
+	off_t size;		/* file size */
 };
 
 struct stack {
@@ -28,11 +30,13 @@ struct stack {
 	struct stackdir* top;	/* any directory */
 };
 
-status_t traverse(const char* restrict path, struct hash_table* files, int follow);
+status_t traverse(const char* restrict path, struct hash_table** files, int oflags);
 int free_hent(const void* key, void* value, void* user_data);
 status_t searchdir(struct stack* dirs, DIR* d, struct hash_table** files, int statflags);
-uint64_t hash(const void* key);
+status_t stream_subdir(int fd, const char* path, int oflags, DIR** d);
+uintmax_t hash(const void* key);
 int hcmpent(const void* a, const void* b);
 
-
 #endif
+
+
