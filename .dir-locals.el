50def05166c50ac2db6ff25d24823b6076ec7b60
yunusemrekmc <yek704@aof.anadolu.edu.tr>
2026-02-03 17:32:08 +0300

    private stuff lmao

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((flycheck-gcc-args . nil)
-	 (compile-command . "make -C /home/yunus/.local/src/c/cp/ MODE=debug")))
- (c-ts-mode . ((tags-file-name . "/home/yunus/.local/src/c/cp/TAGS"))))
