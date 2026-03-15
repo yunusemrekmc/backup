064c7708e6f4f5a5ffbd2c9781f9a002b6b494d4
Yunus Emre Kuyumcu <yunusemrekmc@gmail.com>
2026-03-15 23:40:55 +0300

    working state

--- src/status.c
+++ src/status.c
@@ -11,7 +11,7 @@ const char* stmsg(stcode_t c)
 	case ST_INT_ISNULL: return "Got NULL as argument";
 	case ST_WARN_NO_CRYPT: return "No encryption at target";
 	case ST_ERR_OPEN: return "Failed to open file or directory";
-	case ST_ERR_NOMEM: return "Failed to allocate memory";
+	case ST_ERR_MALLOC: return "Failed to allocate memory";
 	case ST_ERR_HASH_CRE: return "Couldn't create a hash table";
 	case ST_ERR_HASH_UPS: return "Couldn't resize the hash table";
 	case ST_ERR_FILERD: return "Failed to read file or directory";
@@ -20,15 +20,12 @@ const char* stmsg(stcode_t c)
 	}
 }
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wcast-qual"
 void sterr(status_t st)
 {
 	if (st.file_target) {
 		fprintf(stderr, "[%s:%d] %s (code=%d, errno=%d, file=%s: %s)\n",
 			st.file, st.line,
 			st.text, st.c, st.sysc, st.file_target, strerror(st.sysc));
-		free((void*)st.file_target);
 		return;
 	}
 		fprintf(stderr, "[%s:%d] %s (code=%d, errno=%d: %s)\n",
@@ -38,4 +35,11 @@ void sterr(status_t st)
 	return;
 }
 
-#pragma GCC diagnostic pop
+void status_free(status_t st)
+{
+	if (st.file_target) {
+		free(st.file_target);
+	}
+
+	return;
+}
